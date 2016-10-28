131968b12490d854d11feefdfd41b98969f255f9
Gleb Linnik <gleb_linnik@icloud.com>
2016-10-28 20:21:59 +0300

    First version with part of long arithmetic class implementation

---
+++ LongBin.hpp
@@ -0,0 +1,30 @@
+//
+//  LongBin.hpp
+//  Dop*Task1
+//
+//  Created by Глеб Линник on 27.10.16.
+//  Copyright © 2016 Linkin Industris. All rights reserved.
+//
+
+#ifndef LongBin_hpp
+#define LongBin_hpp
+
+#include <stdio.h>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+class LongBin {
+private:
+     deque<short> binDeque;
+public:
+    LongBin();
+    LongBin(deque<short>);
+    friend void setString(LongBin& ,string);
+    friend string getString(LongBin&);
+    LongBin operator+(LongBin);
+    void operator+=(LongBin);
+};
+
+#endif /* LongBin_hpp */
---
+++ main.cpp
@@ -0,0 +1,46 @@
+//
+//  main.cpp
+//  Dop*Task1
+//
+//  Created by Глеб Линник on 27.10.16.
+//  Copyright © 2016 Linkin Industris. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <time.h>
+#include "LongBin.hpp"
+
+using namespace std;
+
+
+int main() {
+    /*int n;
+    cin >> n;
+    clock_t tStart = clock();
+    int sum = 0, dec;
+    unsigned long int bin = 0;
+    
+    for (int i = 0; n > sum; i++) {
+        dec = i;
+        bin = 0;
+        for (int j = 0; dec > 0; j++) {
+            bin += (dec % 2) * powl(10, j);
+            dec /= 2;
+        }
+        
+        int binCount = 0;
+        unsigned long int bin2 = bin;
+        for (; bin2 > 0; binCount++) {
+            bin2 /= 10;
+        }
+        
+        if ((bin % (int)powl(2, binCount)) == i) {
+            sum++;
+        }
+    }
+    cout << bin << "\nExecutional time: " << (double)(clock() - tStart)/CLOCKS_PER_SEC << "s" << endl;*/
+    
+    return 0;
+}
